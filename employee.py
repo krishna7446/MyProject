4ab3b980df80907b2028479cf64081fff607225b
krishna <krishnapanayur@gmail.com>
2021-10-24 13:20:26 +0530

    my_first_commit

---
+++ employee.py
@@ -0,0 +1,14 @@
+class student:
+
+    def __init__(self,employee name,employee id,employee department,employee city):
+        self.employee name =name
+        self.employee id=id
+        self.employee department=department
+        self.employee city=city
+    def display(self):
+        print("name :"+self.employee name)
+        print("name :"+self.employee id)
+        print("name :"+self.employee department)
+        print("name :"+self.employee city)
+        
+
